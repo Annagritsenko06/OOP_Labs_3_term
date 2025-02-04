8b94d97a29cb712764f460d92999552ae81e22ce
Annagritsenko06 <153274338+Annagritsenko06@users.noreply.github.com>
2025-02-04 23:22:13 +0300

    Add files via upload

---
+++ 11 лаба/lab_11/lab_11/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab11
+{
+    public abstract class TelevisionProgram
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+
+        public TelevisionProgram(string title, int duration)
+        {
+            Title = title;
+            Duration = duration;
+        }
+
+        public abstract void ShowDetails();
+
+        public override string ToString()
+        {
+            return $"Телевизионная программа: {Title}, Длительность: {Duration} минут.";
+        }
+    }
+
+
+    public class Director
+    {
+        public string Name { get; set; }
+
+        public Director(string name)
+        {
+            Name = name;
+        }
+        public Director()
+        {
+            Name = "Неизвестный режиссер";
+        }
+        public override string ToString()
+        {
+            return $"Режиссер: {Name}";
+        }
+    }
+
+
+    public class Film : TelevisionProgram
+    {
+        public Director FilmDirector { get; set; }
+
+        public Film(string title, int duration, Director director) : base(title, duration)
+        {
+            FilmDirector = director;
+        }
+        public Film() : base("Неизвестный фильм", 0)
+        {
+            FilmDirector = new Director();
+        }
+        public override void ShowDetails()
+        {
+            Console.WriteLine($"Фильм: {Title}, Длительность: {Duration} минут, Режиссер: {FilmDirector.Name} ShowDetails()");
+        }
+        public override string ToString()
+        {
+            return $"Фильм: {Title}, Длительность: {Duration} минут, Режиссер: {FilmDirector.Name}";
+        }
+        public double Cout(int duration)
+        {
+            double durationInHours = (duration / 60);
+
+            Console.WriteLine($"Длительность фильма в часах: {durationInHours}");
+            return durationInHours;
+            //}
+        }
+
+        public static class Reflector
+        {
+            public static string AssemblyName(string nameOfClass)
+            {
+                Type type = Type.GetType(nameOfClass);
+
+                if (type == null) { return null; }
+                return type.Assembly.FullName;
+
+
+            }
+            public static bool FindPublicConstructors(string nameOfClass)
+            {
+                Type type = Type.GetType(nameOfClass, true);
+                if (type == null) { return false; }
+                var constructors = type.GetConstructors(BindingFlags.Public);
+                return constructors.Any();
+            }
+            public static IEnumerable<string> FindMethods(string nameOfClass)
+            {
+                Type type = Type.GetType(nameOfClass, true);
+                if (type == null) { return Enumerable.Empty<string>(); }
+                var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+                return methods.Select(m => m.Name);
+
+            }
+            public static IEnumerable<string> FindInterfaces(string nameOfClass)
+            {
+                Type type = Type.GetType(nameOfClass, true);
+                if (type == null) { return Enumerable.Empty<string>(); }
+                var interfaces = type.GetInterfaces();
+                return interfaces.Select(m => m.Name);
+            }
+            public static IEnumerable<string> AllFieldsAndProperties(string nameOfClass)
+            {
+                Type type = Type.GetType(nameOfClass, true);
+                if (type == null) { return Enumerable.Empty<string>(); }
+                var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+                var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+                var collection = new List<string>();
+                collection.AddRange(fields.Select(m => $"Field: {m.Name}"));
+                collection.AddRange(properties.Select(m => $"Property: {m.Name}"));
+                return collection;
+            }
+
+            public static IEnumerable<string> FindMethodsWithParameterType(string className, string parameterType)
+            {
+                Type type = Type.GetType(className);
+                if (type == null) return Enumerable.Empty<string>();
+                var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+
+                Type paramType = Type.GetType(parameterType);
+                if (paramType == null) return Enumerable.Empty<string>();
+
+                var result = methods.Where(m => m.GetParameters().Any(p => p.ParameterType == paramType))
+                                    .Select(m => m.Name);
+
+                return result;
+            }
+
+
+            public static void Invoke(string className, string methodName, string parametersFilePath)
+            {
+                Type type = Type.GetType(className);
+                if (type == null)
+                {
+                    Console.WriteLine("Класс не найден.");
+                    return;
+                }
+
+                MethodInfo method = type.GetMethod(methodName);
+                if (method == null)
+                {
+                    Console.WriteLine("Метод не найден.");
+                    return;
+                }
+
+                var parameters = ReadParametersFromFile(parametersFilePath, method);
+
+                var instance = Activator.CreateInstance(type);
+
+                method.Invoke(instance, parameters);
+            }
+
+            private static object[] ReadParametersFromFile(string parametersFilePath, MethodInfo method)
+            {
+                if (!File.Exists(parametersFilePath))
+                    throw new FileNotFoundException($"Файл '{parametersFilePath}' не найден.");
+
+                var lines = File.ReadAllLines(parametersFilePath);
+                var parameters = new List<object>();
+
+                var methodParams = method.GetParameters();
+                for (int i = 0; i < methodParams.Length; i++)
+                {
+                    var paramType = methodParams[i].ParameterType;
+                    var value = Convert.ChangeType(lines[i], paramType);
+                    parameters.Add(value);
+                }
+
+                return parameters.ToArray();
+            }
+
+            public static void SaveToTextFile(string className, string filePath)
+            {
+                var analysisResult = new List<string>
+            {
+                $"Class: {className}",
+                $"Assembly Name: {AssemblyName(className)}",
+                $"Has Public Constructors: {FindPublicConstructors(className)}",
+                "Public Methods:"
+            };
+                analysisResult.AddRange(FindMethods(className).Select(method => $"- {method}"));
+                analysisResult.Add("Fields and Properties:");
+                analysisResult.AddRange(AllFieldsAndProperties(className).Select(f => $"- {f}"));
+                analysisResult.Add("Implemented Interfaces:");
+                analysisResult.AddRange(FindInterfaces(className).Select(i => $"- {i}"));
+
+                File.WriteAllLines(filePath, analysisResult);
+            }
+
+            public static object Create(string className)
+            {
+
+                Type type = Type.GetType(className);
+                Console.WriteLine(type);
+                if (type == null)
+                {
+                    Console.WriteLine($"Тип '{className}' не найден. Проверьте имя класса и наличие сборки.");
+                    return null;
+                }
+
+                var constructor = type.GetConstructor(Type.EmptyTypes);
+                if (constructor == null)
+                {
+                    Console.WriteLine($"Класс '{className}' не имеет конструктора без параметров.");
+                    return null;
+                }
+                object newObject = Activator.CreateInstance(type);
+                return newObject;
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Director director = new Director("Дана Леду Миллер");
+                Film film = new Film("Моана 2", 120, director);
+                var typeOfClass = film.GetType().FullName;
+
+
+                Console.WriteLine("Assembly Name: " + Reflector.AssemblyName(typeOfClass));
+                Console.WriteLine("Has Public Constructors: " + Reflector.FindPublicConstructors(typeOfClass));
+                Console.WriteLine("Public Methods: " + string.Join(", ", Reflector.FindMethods(typeOfClass)));
+                Console.WriteLine("Fields and Properties: " + string.Join(", ", Reflector.AllFieldsAndProperties(typeOfClass)));
+                Console.WriteLine("Implemented Interfaces: " + string.Join(", ", Reflector.FindInterfaces(typeOfClass)));
+                Console.WriteLine("Methods with string parameter: " +
+                                  string.Join(", ", Reflector.FindMethodsWithParameterType(typeOfClass, typeof(string).FullName)));
+
+
+                var demoInstance = Reflector.Create(typeOfClass);
+
+                Console.WriteLine(demoInstance);
+
+
+                File.WriteAllLines("params.txt", new string[] {  "60" });
+                Reflector.Invoke(typeOfClass, "Cout", "params.txt");
+            }
+        }
+
+    }
+}
---
+++ 12 лаба/Lab_12/Lab_12/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using System.IO.Compression;
+using System.Diagnostics;
+namespace Lab12
+{
+    class GAALog
+    {
+        private string FilePath = "GAAlogfile.txt";
+        public void WriteToFile(string action,string details)
+        {
+            string information = $"{DateTime.Now}: Action - {action}, details - {details}";
+            using (StreamWriter sw = new StreamWriter(FilePath, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(information);
+            }
+        }
+        public List<string> ReadFromFile()
+        {
+            if(!File.Exists(FilePath))
+            {
+                return new List<string>();
+            }
+            return File.ReadAllLines(FilePath).ToList();
+        }
+        public List<string> SearchInfo(string info)
+        {
+            return File.ReadAllLines(FilePath).Where(i => i.Contains(info)).ToList();
+        }
+        public void DisplaySearchResults(string info)
+        {
+            List<string> results = SearchInfo(info);
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine("Ничего не найдено.");
+            }
+            else
+            {
+                Console.WriteLine("Найденные записи:");
+                foreach (var result in results)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+        }
+        public List<string> SearchByDate(DateTime startDate, DateTime endDate)
+        {
+            return File.ReadAllLines(FilePath)
+                .Where(line => DateTime.TryParse(line.Substring(0, line.IndexOf(':')), out DateTime logDate) &&
+                               logDate >= startDate && logDate <= endDate).ToList();
+        }
+
+        public int CountRecords()
+        {
+            return File.ReadAllLines(FilePath).Length;
+        }
+
+        public void RemoveOldRecords()
+        {
+            var currentHour = DateTime.Now.Hour;
+            var records = File.ReadAllLines(FilePath)
+                .Where(line => DateTime.TryParse(line.Substring(0, line.IndexOf(':')), out DateTime logDate) &&
+                               logDate.Hour == currentHour)
+                .ToList();
+
+            File.WriteAllLines(FilePath, records);
+        }
+    }
+    class GAADiskInfo
+    {
+        public void FreeSpace()
+        {
+           
+            DriveInfo [] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Свободное месте на диске - {drive.AvailableFreeSpace}");
+            }
+        }
+        public void FileSystem()
+        {
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine(drive.Name);
+                Console.WriteLine($"Имя файловой системы - {drive.DriveFormat}");
+            }
+        }
+        public void InfoAboutDrivers() {
+            DriveInfo [] drives = DriveInfo.GetDrives();
+        foreach(DriveInfo drive in drives){
+                
+                if (drive.IsReady)
+                {
+                    Console.WriteLine(drive.Name);
+                    Console.WriteLine($"Объем диска - {drive.TotalSize}");
+                    Console.WriteLine($"Свободное пространство - {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка диска - {drive.VolumeLabel}");
+                }
+        }
+            }
+
+    }
+    class GAAFileInfo
+    {
+        private string FilePath = "GAAlogfile.txt";
+        public void FileInfo()
+        {
+            
+        FileInfo file = new FileInfo(FilePath);
+            if (file.Exists)
+            {
+                Console.WriteLine($"Полное имя файла: {file.FullName}");
+                Console.WriteLine($"Размер файла: {file.Length}");
+                Console.WriteLine($"Расширение файла: {file.Extension}");
+                Console.WriteLine($"Имя файла: {file.Name}");
+                Console.WriteLine($"Дата создания файла: {file.CreationTime}");
+                Console.WriteLine($"Информация об изменениях: {file.LastWriteTime}");
+
+            }
+        }
+    }
+    class GAADirInfo
+    {
+        public void DirInfo(string nameOdDir)
+        {
+            if (Directory.Exists(nameOdDir))
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(nameOdDir);
+                Console.WriteLine($"Имя директория: {dirInfo.FullName} ");
+                Console.WriteLine($"Количестве файлов: {dirInfo.GetFiles().Length} ");
+                Console.WriteLine($"Время создания: {dirInfo.CreationTime} ");
+                Console.WriteLine($"Количестве поддиректориев: {dirInfo.GetDirectories().Length}");
+                Console.WriteLine($"Список родительских директориев: {dirInfo.Parent?.FullName}");
+            }
+        }
+    }
+    class GAAFileManager
+    {
+        public void Method_1(string directory)
+        {
+
+            string inspectDir = Path.Combine(directory, "GAAInspect");
+            Directory.CreateDirectory(inspectDir);
+            string dirInfoFile = Path.Combine(inspectDir, "GAAdirinfo.txt");
+
+            using (StreamWriter sw = new StreamWriter(dirInfoFile))
+            {
+                foreach (var file in Directory.GetFiles(directory))
+                {
+                    sw.WriteLine(file);
+                }
+            }
+
+            string fileToCopy = Directory.GetFiles(directory).FirstOrDefault();
+            if (fileToCopy != null)
+            {
+                string copiedFile = Path.Combine(inspectDir, "CopiedFile" + Path.GetExtension(fileToCopy));
+                File.Copy(fileToCopy, copiedFile);
+                File.Delete(fileToCopy);
+            }
+        }
+
+        public void Method_2(string newDir, string directory, string extention)
+        {
+
+            Directory.CreateDirectory(newDir);
+            foreach (var file in Directory.GetFiles(directory, $"*{extention}"))
+            {
+                var path = Path.Combine(newDir, Path.GetFileName(file));
+                File.Copy(file, path);
+
+            }
+
+        }
+        public void Method_3(string directory, string dir2)
+        {
+            try
+            {
+                
+                if (!Directory.Exists(directory))
+                {
+                    Console.WriteLine("Каталог-источник не существует.");
+                    return;
+                }
+
+                if (Directory.Exists(dir2))
+                {
+                    Console.WriteLine("Каталог назначения уже существует.");
+                    return;
+                }
+
+                Directory.Move(directory, dir2);
+                Console.WriteLine($"Каталог успешно перемещён из {directory} в {dir2}.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при перемещении каталога: {ex.Message}");
+            }
+        }
+
+        public void Method_4(string directory, string nameZip)
+        {
+            ZipFile.CreateFromDirectory(directory, nameZip);
+        }
+        public void Method_5(string directory, string nameZip)
+        {
+            ZipFile.ExtractToDirectory(nameZip, directory);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GAALog gAALog = new GAALog();
+            gAALog.WriteToFile("Создан файл", " с именем GAAlogfile.txt ");
+            gAALog.ReadFromFile();
+            gAALog.DisplaySearchResults("Stream");
+            GAAFileInfo info = new GAAFileInfo();
+            info.FileInfo();
+            GAADiskInfo diskInfo = new GAADiskInfo();
+            diskInfo.FreeSpace();
+            diskInfo.FileSystem();
+            diskInfo.InfoAboutDrivers();
+
+            GAADirInfo dirInfo = new GAADirInfo();
+            dirInfo.DirInfo("C:\\Users\\admin\\Documents\\уник\\3 СЕМ");
+
+            GAAFileManager fileManager = new GAAFileManager();
+            fileManager.Method_1("C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\папка_для_лабы");
+            fileManager.Method_2("C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\папка_для_лабы\\newDir", "C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\папка_для_лабы\\GAAInspect", "txt");
+            fileManager.Method_3("C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\папка_для_лабы\\GAAInspect", "C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\папка_для_лабы\\newDir");
+            fileManager.Method_4("C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\папка_для_лабы", "Dir.zip");
+            fileManager.Method_5("C:\\Users\\admin\\Documents\\уник\\3 СЕМ\\ООП\\12 лаба\\Lab_12\\Lab_12\\Новая папка", "Dir.zip");
+            DateTime startDate = new DateTime(2024, 12, 17, 0, 0, 0);
+            DateTime endDate = new DateTime(2024, 12, 17, 23, 59, 59);
+            var records = gAALog.SearchByDate(startDate, endDate);
+            Console.WriteLine($"Записи за {startDate.ToShortDateString()}: {records.Count}");
+
+            int recordCount = gAALog.CountRecords();
+            Console.WriteLine($"Общее количество записей: {recordCount}");
+
+            gAALog.RemoveOldRecords();
+            Console.WriteLine("Старые записи удалены, оставлены только записи за текущий час.");                                                                                                           //6 задание с файлами не сделано
+        }
+    }
+}
